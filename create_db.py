bc4910534c54cccee17294c1765edb11947c538f
aliapin <aleksandr.liapin@gmail.com>
2021-12-26 10:52:41 +0100

    create table script added

--- create_db.py
+++ create_db.py
@@ -7,6 +7,27 @@ API_SECRET = 'bHq0vgFiZhuWL9l0FT1iWAsPngt35YkV3GTXUkfY'
 
 connection = sqlite3.connect('app.db')
 cursor = connection.cursor()
+# cursor.execute("""
+#     CREATE TABLE IF NOT EXISTS stock (
+#         id INTEGER PRIMARY KEY, 
+#         symbol TEXT NOT NULL UNIQUE, 
+#         company TEXT NOT NULL
+#     )
+# """)
+# cursor.execute("""
+#     CREATE TABLE IF NOT EXISTS stock_price (
+#         id INTEGER PRIMARY KEY, 
+#         stock_id INTEGER,
+#         date NOT NULL,
+#         open NOT NULL, 
+#         high NOT NULL, 
+#         low NOT NULL, 
+#         close NOT NULL, 
+#         adjusted_close NOT NULL, 
+#         volume NOT NULL,
+#         FOREIGN KEY (stock_id) REFERENCES stock (id)
+#     )
+# """)
 
 api = tradeAPI.REST(API_KEY, API_SECRET, API_ENDPOINT)
 assets = api.list_assets()
