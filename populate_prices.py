9ce207a9606de72f2d1e0b875a3b7aebc3187587
aliapin <aleksandr.liapin@gmail.com>
2021-12-27 15:35:57 +0100

    stock_prices populaton added

--- populate_prices.py
+++ populate_prices.py
@@ -3,9 +3,29 @@ import connect
 connection, cursor = connect.getDbConnect()
 api = connect.getAlpacaAPIconnect()
 
+symbols = []
+stock_dict = {}
+symbols = cursor.execute("SELECT id, symbol FROM stock").fetchall()
+
+for symbol in symbols:
+    stock_dict[symbol['symbol']] = symbol['id']
+
 # Get daily price data for AAPL over the last 5 trading days.
 barset = api.get_barset(['AAPL', 'MSFT'], 'day', limit=5)
-for bars in barset:
-    print(bars)
+
+chunk_size = 200
+
+
+for symbol in barset:
+    for bar in barset[symbol]:
+        if symbol in stock_dict.keys():
+            bar_id = stock_dict[symbol]
+        else:
+            print(f'No stock_id for {symbol} found!')
+            continue
+        
+        cursor.execute(f'INSERT INTO stock_price (stock_id, date, open, high, low, close, volume) VALUES (?, ?, ?, ?, ?, ?, ?)', 
+                       (int(bar_id), bar.t.date(), bar.o, bar.h, bar.l, bar.c, bar.v))
+        print(f'{symbol}: inserted')
     
-# connection.commit()
+connection.commit()
--- populate_stocks.py
+++ populate_stocks.py
@@ -2,7 +2,7 @@ import connect
 
 connection, cursor = connect.getDbConnect()
 
-cursor.execute("SELECT symbol, company FROM stock")
+cursor.execute("SELECT symbol, name FROM stock")
 rows = cursor.fetchall()
 symbols = [row['symbol'] for row in rows]
 
@@ -12,7 +12,7 @@ assets = api.list_assets()
 for asset in assets:
     try:
         if asset.status == 'active' and asset.tradable and asset.symbol not in symbols:
-            cursor.execute("INSERT INTO stock (symbol, company) VALUES (?, ?)", (asset.symbol, asset.name))
+            cursor.execute("INSERT INTO stock (symbol, name) VALUES (?, ?)", (asset.symbol, asset.name))
             print (f"New asset: {asset.symbol} was inserted")
     except Exception as e:
         print(asset.name)
